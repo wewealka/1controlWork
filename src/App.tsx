918928256dceef9a3db02a0f7ffab9a225eef61f
wewealka228 <wewepoweralka@mail.ru>
2024-11-22 02:11:34 +0300

    changed backgCol && add setters in main App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
 import './App.css';
 import { ControlWork } from './control_work/ControlWork';
+import { Setter } from './setters/Setter';
 
 function App() {
   return (
     <div className="App">
+      <Setter/>
       <ControlWork/>
     </div>
   );
